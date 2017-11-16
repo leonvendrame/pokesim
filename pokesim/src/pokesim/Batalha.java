e50300e45f0f5b24a8a10b62c5b98fc396166e55
Leonardo Vendrame <leonvendrame@gmail.com>
2017-11-16 13:48:45 +0000

    Verificação dos argumentos

--- pokesim/src/pokesim/Batalha.java
+++ pokesim/src/pokesim/Batalha.java
@@ -1,5 +1,7 @@
 package pokesim;
 
+import jdk.nashorn.internal.scripts.JO;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -7,7 +9,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Batalha {
-    private List<Jogador> jogadores;
+    private Jogador jogador0 = null;
+    private Jogador jogador1 = null;
     private static String[][] tabelaEspecie;
     private static String[][] tabelaAtaque;
 
@@ -100,9 +103,175 @@ public class Batalha {
         return tabelaAtaque;
     }
 
-    public void inicializarJogadores() {
-        jogadores = Arrays.asList(new Jogador[2]);
+    public int checarArgumentos(int[] args) {
+        /* Verifica a quantidade de argumentos;
+           Caso o usuário entre com argumentos suficientes para 1 time, ele inicializará este time;
+           Caso o usuário entre com argumentos suficientes para 2 times, ele inicializará estes 2 times;
+           Caso o usuário entre com mais argumentos do que o necessário e os mesmos são suficientes para 1 time, os
+           argumentos a mais serão desconsiderados, ainda inicializando o time;
+           Caso o usuário entre com mais argumentos do que o necessário e os mesmos são suficientes para 2 times, os
+           argumentos a mais serão desconsiderados, ainda inicializando os times;
+         */
+
+        if (args.length < 2 || args[1] == 0) {
+            return 0;
+        }
+
+        int qtdeArgsJog = ((args[1] * 6) + 2);
+
+        if (args.length >= qtdeArgsJog) {
+            if (args.length == qtdeArgsJog || args.length == (qtdeArgsJog + 1)) {
+                return 1;
+            } else {
+                if (args[qtdeArgsJog + 1] == 0 || args.length < qtdeArgsJog + (((args[qtdeArgsJog + 1]) * 6) + 2)) {
+                    return 1;
+                } else if (args.length >= qtdeArgsJog + (((args[qtdeArgsJog + 1]) * 6) + 2)) {
+                    return 2;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean validaCamposArg(int[] args) {
+        /*  Verifica se os campos são válidos, seja algum deles igual a 0 então os argumentos não são válidos
+            Só não se aplica ao jogador que pode ser maquina ou humano;
+         */
+        boolean valido = true;
+        int segJogador = ((args[1] * 6) + 2), i = 2;
+
+        if (args[1] == 0) {
+            valido = false;
+        }
+
+         while (valido && i < segJogador) {
+            if (args[i] == 0 || args[i + 1] == 0){
+                valido = false;
+            }
+            i += 6;
+        }
+
+        if (args.length > segJogador + 1 && args.length < (segJogador + ((args[segJogador + 1] * 6) + 2))) {
+            if (args[segJogador + 1] == 0) {
+                    valido = false;
+            }
+
+            i += 2;
+
+            while (valido && i < args.length) {
+                if (args[i] == 0 || args[i + 1] == 0) {
+                    valido = false;
+                }
+                i += 6;
+            }
+        }
+
+        return valido;
+    }
+
+    public boolean verificaTamanhoArt(int[] args) {
+        if (args.length < 8) {
+            return false;
+        }
+        if (args.length < ((args[1] * 6) + 2)){
+            return false;
+        }
+        return true;
+    }
+
+    public void inicializarJogadores(int[] args) {
+        int nJogadores = 0;
+
+        if (verificaTamanhoArt(args) && validaCamposArg(args)) {
+            nJogadores = checarArgumentos(args);
+        } else {
+            System.out.printf("\nSeus argumentos são inválidos\n");
+            System.out.printf("O numero de pokemons deve corresponder às suas inicializações.\n");
+            System.out.printf("O níveis e IDs de pokemons e ataques não podem ser iguais a 0.\n");
+        }
+
+        if (nJogadores == 2) {
+            int argsSegundoJogador = ((args[1] * 6) + 2);
+
+            //JOGADOR 1
+
+            if (args[0] == 0) {
+                this.jogador0 = new Maquina();
+            } else {
+                this.jogador0 = new Humano();
+            }
+
+            int j = 2;
+            for (int i = 0; i < args[1]; i++) {
+                Pokemon addPokemon = new Pokemon(args[j], args[j+1]);
+                this.jogador0.getTime().add(addPokemon);
+                j += 6;
+            }
+
+            System.out.printf("\n\t\tTime - Jogador 1 - %s\n", jogador0.getClass().getTypeName() == "pokesim.Humano" ? "Humano" : "Máquina" );
+            for (Pokemon pokemon : getJogador0().getTime()) {
+                System.out.printf("%-14s LVL %4s\n", pokemon.getEspecie().getNome(), pokemon.getLevel());
+            }
+
+            // JOGADOR 2
+
+            if (args[argsSegundoJogador] == 0) {
+                this.jogador1 = new Maquina();
+            } else {
+                this.jogador1 = new Humano();
+            }
+
+            j = argsSegundoJogador + 2;
+            for (int i = 0; i < args[argsSegundoJogador + 1]; i++) {
+                Pokemon addPokemon = new Pokemon(args[j], args[j+1]);
+                this.jogador1.getTime().add(addPokemon);
+                j += 6;
+            }
+
+            System.out.printf("\n\t\tTime - Jogador 2 - %s\n", jogador1.getClass().getTypeName() == "pokesim.Humano" ? "Humano" : "Máquina" );
+            for (Pokemon pokemon : getJogador1().getTime()) {
+                System.out.printf("%-14s LVL %4s\n", pokemon.getEspecie().getNome(), pokemon.getLevel());
+            }
+
+            return;
+        }
+
+        if (nJogadores == 1) {
+            if (args[0] == 0) {
+                this.jogador0 = new Maquina();
+            } else {
+                this.jogador0 = new Humano();
+            }
+
+            int j = 2;
+            for (int i = 0; i < args[1]; i++) {
+                Pokemon addPokemon = new Pokemon(args[j], args[j+1]);
+                this.jogador0.getTime().add(addPokemon);
+                j += 6;
+            }
+
+            System.out.printf("\n\t\tTime - Jogador 1 - %s\n", jogador0.getClass().getTypeName() == "pokesim.Humano" ? "Humano" : "Máquina" );
+            for (Pokemon pokemon : getJogador0().getTime()) {
+                System.out.printf("%-15s LVL %s \n", pokemon.getEspecie().getNome(), pokemon.getLevel());
+            }
+
+            return;
+        }
+
+        if (nJogadores == 0) {
+            System.out.printf("Inicializando Jogadores Manualmente\n");
+            return;
+        }
+
         return;
+    }
+
+    public Jogador getJogador0() {
+        return jogador0;
+    }
+
+    public Jogador getJogador1() {
+        return jogador1;
     }
 
     public void executarTurno() {
--- pokesim/src/pokesim/Main.java
+++ pokesim/src/pokesim/Main.java
@@ -1,83 +1,26 @@
 package pokesim;
 
+import sun.rmi.server.InactiveGroupException;
+
 import java.util.List;
 
 import static pokesim.Batalha.carregarTabelas;
 
 public class Main {
 
-    public static int checarArgumentos(String[] args) {
-        int indiceQtdePokemon = 0;
-        if (args.length > 1 && args.length >= ((Integer.parseInt(args[1]) * 6) + 2)) {
-            indiceQtdePokemon = (((Integer.parseInt(args[1])) * 6) + 2);
-            if (args.length == indiceQtdePokemon) {
-                return 1;
-            } else {
-                if (args.length <= ( indiceQtdePokemon + ((Integer.parseInt(args[indiceQtdePokemon]) * 6) + 2)))
-                    return 0;
-                if (args.length == ( indiceQtdePokemon + ((Integer.parseInt(args[indiceQtdePokemon + 2]) * 6) + 2))) {
-                    return 2;
-                }
-            }
-        }
-        return 0;
-    }
-
     public static void main(String[] args) {
+        int argsInt[] = new int[args.length];
+
         carregarTabelas();
 
-        Jogador jogador0 = null;
-        Jogador jogador1 = null;
-
-
-
-        if (checarArgumentos(args) == 1) {
-            if (args[0] == "0") {
-                jogador0 = new Maquina();
-            } else {
-                jogador0 = new Humano();
-            }
-
-            int j = 2;
-            while (j < args.length) {
-                for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-                    Pokemon pokemon = new Pokemon(Integer.parseInt(args[j]), Integer.parseInt(args[j + 1]));
-                    jogador0.adicionaPokemonTime(pokemon);
-                    j += 6;
-                }
-            }
-        } else if (checarArgumentos(args) == 2) {
-            System.out.println("Dois Jogadores Exatos");
-//            if (args[0] == "0") {
-//                jogador0 = new Maquina();
-//            } else {
-//                jogador0 = new Humano();
-//            }
-//
-//            int j = 2;
-//            while (j < args.length) {
-//                for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-//                    Pokemon pokemon = new Pokemon(Integer.parseInt(args[j]), Integer.parseInt(args[j + 1]));
-//                    jogador0.adicionaPokemonTime(pokemon);
-//                    j += 6;
-//                }
-//            }
-        } else {
-//            Pokemon meu = new Pokemon(6, 1);
-//
-//            Pokemon meu2 = new Pokemon(151, 2);
-//
-//            meu.getInfoPokemon();
-//
-//            meu2.getInfoPokemon();
+        for (int i = 0; i < args.length; i++) {
+            argsInt[i] = Integer.parseInt(args[i]);
+            System.out.printf("%d ", argsInt[i]);
         }
 
-        if (jogador0 != null){
-            List<Pokemon> poketime = jogador0.getTime();
-            for (Pokemon poke : poketime) {
-                poke.getInfoPokemon();
-            }
-        }
+        Batalha pokeBatalha = new Batalha();
+
+        pokeBatalha.inicializarJogadores(argsInt);
 
     }
 }
